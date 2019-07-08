5b17d61298271c10e78579315349140c7ac34eb9
AlexeyQWE <mountain.skier@yandex.ru>
2019-07-08 22:06:07 +0700

    Complete Reflextion test

--- Reflection/src/main/java/eltex/tasks/Main.java
+++ Reflection/src/main/java/eltex/tasks/Main.java
@@ -1,16 +1,27 @@
-package main.java.eltex.tasks;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
+package eltex.tasks;
+import java.lang.reflect.*;
 
-public class Main {
+public class Main { 
 
-    public static void ShowPhones (Object myClass) {
+    //Доступ к приватным методам
+
+     public static void getName (Object myClass) {
 
         try {
-            Method met = myClass.getClass().getDeclaredMethod("ShowNumber");
+            Method met = myClass.getClass().getDeclaredMethod("getName");
             met.setAccessible(true);
-            met.invoke(myClass);
+           System.out.print(met.invoke(myClass) + ": ");
+        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
+            e.printStackTrace();
+        }
+    }
+
+     public static void getNumber (Object myClass) {
+
+        try {
+            Method met = myClass.getClass().getDeclaredMethod("getNumber");
+            met.setAccessible(true);
+           System.out.println(met.invoke(myClass) + "\n");
         } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException e) {
             e.printStackTrace();
         }
@@ -18,35 +29,77 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Student ref = new Student();
+        Student std = new Student("Gena", "+71421421542");
         String name =  null;
         String number =  null;
 
+        // Доступ к приватным полям
+
         try {
-            Field fl = ref.getClass().getDeclaredField("name");
+            Field fl = std.getClass().getDeclaredField("name"); 
             fl.setAccessible(true);
-            name = (String) fl.get(ref);
-            System.out.println("Student 1 - " + name);
-            fl.set(ref, (String) "Alexey");
-            name = (String) fl.get(ref);
+            name = (String) fl.get(std);
+            System.out.print("Student 1 - ");
+            getName(std);
+            fl.set(std, (String) "Alexey");
+            name = (String) fl.get(std);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        ShowPhones(ref);
-        System.out.println("Student 2 - " + name);
+        getNumber(std);
+
+        System.out.print("Student 2 - ");
+        getName(std);
 
         try {
-            Field fl = ref.getClass().getDeclaredField("number");
+            Field fl = std.getClass().getDeclaredField("number");
             fl.setAccessible(true);
-            fl.set(ref, (String) "+71231231212");
-            number = (String) fl.get(ref);
+            fl.set(std, (String) "+71231231212");
+            number = (String) fl.get(std);
         } catch (NoSuchFieldException | IllegalAccessException e) {
             e.printStackTrace();
         }
 
-        ShowPhones(ref);
+        getNumber(std);
 
+
+        // экземпляр класса с помощью рефлексии :
+
+        Student std2  = null;
+        Class obj = null;
+
+        try {                                               // Для конструктора по умолчанию
+            obj = Class.forName(Student.class.getName());
+            std2 = (Student) obj.newInstance();
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                     e.printStackTrace();
+        }
+
+        System.out.println(std2);
+        getName(std2);
+        getNumber(std2);
+
+        try {                                             //Для конструктора с параметрами
+            obj = Class.forName(Student.class.getName());
+            Class [] obj2 = {String.class, String.class};
+            std2 = (Student) obj.getConstructor (obj2).newInstance("Sergey", "+74351240912");
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException 
+                | NoSuchMethodException | InvocationTargetException e) {
+                     e.printStackTrace();
+        }
+
+        System.out.println(std2);
+        getName(std2);
+        getNumber(std2);
+
+        Constructor [] cons = obj.getConstructors(); // Информация о параметрах конструктора
+        for (Constructor constr : cons) {
+             Class[] paramTypes = constr.getParameterTypes();
+            for (Class paramType : paramTypes) {
+                 System.out.print(paramType.getName() + " ");
+             }
+        }
 
     }
 }
--- Reflection/src/main/java/eltex/tasks/Student.java
+++ Reflection/src/main/java/eltex/tasks/Student.java
@@ -1,11 +1,29 @@
-package main.java.eltex.tasks;
+package eltex.tasks;
 
 public class Student {
 
-    private String name = "Gena";
-    private String number = "+71421421542";
+    private String name;
+    private String number;
 
-    private void ShowNumber () {
-        System.out.println("Phones is - " + number + '\n');
+    public Student () {
+
+    	this.name = "Gena";
+    	this.number = "+71421421542";
+    };
+
+    public Student (String name, String number) {
+
+    	this.name = name;
+    	this.number = number;
+    }
+
+    private String getName() {
+
+    	return this.name;
+    }
+
+    private String getNumber() {
+
+    	return this.number;
     }
 }
