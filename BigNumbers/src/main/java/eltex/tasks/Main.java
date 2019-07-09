875a36eafafbb677c83beb8a3f1e89d8b1fe3e4c
AlexeyQWE <mountain.skier@yandex.ru>
2019-07-09 21:33:43 +0700

    Pull BigNumbers

---
+++ BigNumbers/src/main/java/eltex/tasks/Main.java
@@ -0,0 +1,40 @@
+package eltex.tasks;
+import java.math.*;
+
+class Main {
+
+	public static void main(String[] args) {
+
+		BigInteger num = BigInteger.valueOf(2);
+
+		for (int i = 1; i < 100; ++i) {		
+		 	num = num.multiply(BigInteger.valueOf(2));
+		}
+
+		System.out.println("BigInteger");
+		System.out.println("2^100 = " + num);
+
+		for (int i = 1; i < 1000; ++i) {		
+		 	num = num.multiply(BigInteger.valueOf(2));
+		}
+
+		System.out.println("2^1000 = " + num + "\n\n");
+
+
+		BigDecimal num1 = BigDecimal.valueOf(2.0);
+
+		for (int i = 1; i < 100; ++i) {		
+		 	num1 = num1.multiply(BigDecimal.valueOf(2.0));
+		}
+
+		System.out.println("BigDecimal");
+		System.out.println("2^100 = " + num1);
+
+		for (int i = 1; i < 1000; ++i) {		
+		 	num1 = num1.multiply(BigDecimal.valueOf(2));
+		}
+
+		System.out.println("2^1000 = " + num1);
+
+	}
+}
