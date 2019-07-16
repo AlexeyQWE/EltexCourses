7854915cdf08352f24bad7adea0d41b70bf5c922
AlexeyQWE <mountain.skier@yandex.ru>
2019-07-16 20:38:49 +0700

    fix syntax

--- Client-Server/TCP/Server/src/main/java/eltex/tasks/Server.java
+++ Client-Server/TCP/Server/src/main/java/eltex/tasks/Server.java
@@ -2,7 +2,7 @@ package eltex.tasks;
 
 import java.io.*;
 import java.net.*;
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
