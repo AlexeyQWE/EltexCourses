9167d6839557365d422d9aa7e07dd34c90e736a3
AlexeyQWE <mountain.skier@yadnex.ru>
2019-07-18 15:12:03 +0700

    Pull ExecuteQuery

---
+++ ExecuteQuery/src/main/java/eltex/tasks/ExecuteQuery.java
@@ -0,0 +1,32 @@
+package eltex.tasks;
+
+import java.sql.*;
+
+public class ExecuteQuery {
+
+    private static final String DB_URL = "host";
+
+    public static void main(String[] args) {
+
+        String id = null, fio = null, phone = null;
+
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, "login", "NOT_A_PASSWORD_ACTUALLY");
+            Statement statement = connection.createStatement();
+            statement.executeUpdate("INSERT INTO developer VALUES (4, 'Ramus', '+79529344014')");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM developer");
+
+            while (resultSet.next()) {
+                id = resultSet.getString("id");
+                fio = resultSet.getString("fio");
+                phone = resultSet.getString("phone");
+            }
+            connection.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println("id: " + id);
+        System.out.println("fio: " + fio);
+        System.out.println("phone " + phone);
+    }
+}
