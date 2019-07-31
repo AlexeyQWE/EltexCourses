fcca1cb7bad359994143044be6e85e8614055ec6
AlexeyQWE <mountain.skier@yandex.ru>
2019-07-31 20:03:58 +0700

    fix

--- WebServer/src/main/java/eltex/tasks/HTTPServer.java
+++ WebServer/src/main/java/eltex/tasks/HTTPServer.java
@@ -14,23 +14,25 @@ public class HTTPServer {
         Scanner scan = new Scanner (file).useDelimiter("\0");
         String html_doc = scan.next();
 
-        String output = "HTTP/1.0 200 OK\nContent-Length:" + html_doc.length() + "\n\n" + html_doc;
+       String output = "HTTP/1.1 200 OK\nContent-Length:" + html_doc.length() + "\n\n" + html_doc;    
 
         ServerSocket server = new ServerSocket(8080);
         while (true) {
             Socket client = server.accept();
             new Thread (() -> {
                 try {
-                     OutputStream outStream = client.getOutputStream(); 
-                     PrintWriter out = new PrintWriter(outStream);
+                    OutputStream outStream = client.getOutputStream(); 
+                    PrintWriter out = new PrintWriter(outStream);
                     out.write(output);
                     out.flush();
+                    client.close();
                 }
                 catch (IOException e) {
                     System.err.println(e.getMessage());
                 }
+
             }).start();         
        }
 
     }
-}
+}   
