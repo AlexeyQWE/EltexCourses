8065c9db004cc9429c057e22ddca9f68406890fa
AlexeyQWE <mountain.skier@yadnex.ru>
2019-08-01 14:20:24 +0700

    Add connect WebServer-db

--- SpringData/src/main/java/eltex/tasks/Main.java
+++ SpringData/src/main/java/eltex/tasks/Main.java
@@ -1,9 +1,12 @@
 package eltex.tasks;
 
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import java.util.Date;
+import java.io.*;
+import java.net.*;
+import java.util.*;
 
 @SpringBootApplication
 public class Main {
@@ -23,8 +26,6 @@ public class Main {
             user2 = new User(2, "Boris", "800");
             CrudRepository.save(user1);
             CrudRepository.save(user2);
-            for (User user : CrudRepository.findAll())
-                System.out.println(user.getFio());
         };
     }
     @Bean
@@ -33,8 +34,53 @@ public class Main {
         return (args) -> {
             MongoRepository.save(new Call(1,  user1.getId(), user2.getId(),date.toString()));
             MongoRepository.save(new Call(2,user2.getId(), user1.getId(), date.toString()));
-            for (Call call : MongoRepository.findAll())
-                System.out.println(call.getId());
+        };
+    }
+    @Bean
+    public CommandLineRunner demo2(UserRepository CrudRepository) {
+        return (args) -> {
+            ServerSocket server = new ServerSocket(8080);
+            while (true) {
+                Socket client = server.accept();
+                new Thread (() -> {
+                    try {
+                        InputStream inStream = client.getInputStream();
+                        Scanner in = new Scanner(inStream);
+                        String client_request = in.nextLine();
+                        String [] arg = client_request.split(" ");
+                        String [] tokens = arg[1].split("/");
+                        String output = null;
+
+                        if (tokens[1].equals("get_users") && tokens.length == 2) {
+                            ObjectMapper mapper = new ObjectMapper();
+                            ArrayList<User> users = new ArrayList<>();
+                            CrudRepository.findAll().forEach(users::add);
+                            output = mapper.writeValueAsString(users);
+                            output = "HTTP/1.1 200 OK\n\n" + output;
+                        }
+
+                        else if (tokens[1].equals("get_user") && tokens.length > 2) {
+                            Integer id = Integer.valueOf(tokens[2]);
+                            ObjectMapper mapper = new ObjectMapper();
+                            User u = CrudRepository.findById(id).get();
+                            output = mapper.writeValueAsString(u);
+                            output = "HTTP/1.1 200 OK\n\n" + output;
+                        }
+                        else {
+                            output = "HTTP/1.1 404";
+                        }
+                        OutputStream outStream = client.getOutputStream();
+                        PrintWriter out = new PrintWriter(outStream);
+                        out.write(output);
+                        out.flush();
+                        client.close();
+                    }
+                    catch (IOException e) {
+                        System.err.println(e.getMessage());
+                    }
+
+                }).start();
+            }
         };
     }
 }
