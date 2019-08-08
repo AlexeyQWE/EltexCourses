d3575bc7bba343b1c1cec714e6dfb058dbaeeb26
AlexeyQWE <mountain.skier@yadnex.ru>
2019-08-08 15:08:27 +0700

    Add javadoc

--- SpringBoot/src/main/java/eltex/tasks/User.java
+++ SpringBoot/src/main/java/eltex/tasks/User.java
@@ -2,10 +2,19 @@ package eltex.tasks;
 
 import lombok.*;;
 
+/**
+ * Класс представления пользователя
+ * @author Alexey Karasev
+ * @version v1.0
+ */
+@NoArgsConstructor
 public class User {
 
+    /** Поле идентификатора */
     @Getter @Setter private Integer id;
+    /** Поле ФИО */
     @Getter @Setter private String fio;
+    /** Поле телефона */
     @Getter @Setter private String phone;
 
     User(Integer id, String fio, String phone) {
--- SpringBoot/src/main/java/eltex/tasks/UserController.java
+++ SpringBoot/src/main/java/eltex/tasks/UserController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.ArrayList;
 
+/**
+ * Класс-контроллер
+ * @author Alexey Karasev
+ * @version v1.0
+ */
 @RestController
 public class UserController {
 
@@ -15,11 +20,19 @@ public class UserController {
     private ArrayList<User> users = new ArrayList<User>();
 
     @RequestMapping("/clear")
+    /**
+     * Метод для сопоставления с точкой входа <b>/clear</b>
+     * @see User#User()
+     */
     public void clear() {
         users.clear();
     }
 
     @RequestMapping("/create")
+    /**
+     * Метод для сопоставления с точкой входа <b>/create</b>
+     * @see User#User()
+     */
     public void create() {
         users.add(new User(1, "Alexey", "900"));
         users.add(new User(2, "Gena", "800"));
@@ -28,18 +41,28 @@ public class UserController {
     }
 
     @RequestMapping("/get_users")
+    /**
+     * Метод для сопоставления с точкой входа <b>/get_users</b>
+     * @return Строка формата JSON
+     * @see User#User()
+     */
     public String getUser() throws IOException {
         if (!users.isEmpty())
             return mapper.writeValueAsString(users);
         else
-            return "Users not find";
+            return "Users not found";
     }
 
     @RequestMapping("/get_user/{id}")
+    /**
+     * Метод для сопоставления с точкой входа <b>/get_user/id</b>
+     * @return Строка формата JSON
+     * @see User#User()
+     */
     public String getUserWithID(@PathVariable("id") Integer id) throws IOException {
         if (!users.isEmpty())
             return mapper.writeValueAsString(users.get(id - 1));
         else
-            return "Current user not find";
+            return "Current user not found";
     }
 }
