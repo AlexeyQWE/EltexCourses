261dbbc5652f56882ae40f4e73ef84263b94d5f4
AlexeyQWE <mountain.skier@yadnex.ru>
2019-08-08 14:41:13 +0700

    Add logger

--- SpringBoot/src/main/java/eltex/tasks/Application.java
+++ SpringBoot/src/main/java/eltex/tasks/Application.java
@@ -1,11 +1,19 @@
 package eltex.tasks;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+
+    private static final Logger log = Logger.getLogger(Application.class);
+
     public static void main(String[] args) {
+        log.info("INFO");
+        log.debug("DEBUG");
+        log.error("ERROR");
+        log.warn("WARNING");
         SpringApplication.run(Application.class, args);
     }
 }
--- SpringBoot/src/main/java/eltex/tasks/UserController.java
+++ SpringBoot/src/main/java/eltex/tasks/UserController.java
@@ -14,16 +14,32 @@ public class UserController {
     private ObjectMapper mapper = new ObjectMapper();
     private ArrayList<User> users = new ArrayList<User>();
 
-    @RequestMapping("/get_users")
-    public String getUser() throws IOException {
+    @RequestMapping("/clear")
+    public void clear() {
+        users.clear();
+    }
+
+    @RequestMapping("/create")
+    public void create() {
         users.add(new User(1, "Alexey", "900"));
         users.add(new User(2, "Gena", "800"));
         users.add(new User(3, "Eugene", "700"));
         users.add(new User(4, "Venc", "600"));
-        return mapper.writeValueAsString(users);
     }
+
+    @RequestMapping("/get_users")
+    public String getUser() throws IOException {
+        if (!users.isEmpty())
+            return mapper.writeValueAsString(users);
+        else
+            return "Users not find";
+    }
+
     @RequestMapping("/get_user/{id}")
     public String getUserWithID(@PathVariable("id") Integer id) throws IOException {
-        return mapper.writeValueAsString(users.get(id - 1));
+        if (!users.isEmpty())
+            return mapper.writeValueAsString(users.get(id - 1));
+        else
+            return "Current user not find";
     }
 }
