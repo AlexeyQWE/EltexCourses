1f78182fb658131807cdaeb4debfa8ed4ae52e31
AlexeyQWE <mountain.skier@yandex.ru>
2019-08-14 20:33:45 +0700

    Add users graphics table

--- JavaFX/src/main/java/eltex/tasks/App.java
+++ JavaFX/src/main/java/eltex/tasks/App.java
@@ -1,43 +1,16 @@
 package eltex.tasks;
 
-import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpClient;
-import io.vertx.core.http.HttpClientOptions;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpVersion;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.Group;
-import javafx.scene.text.Text;
-
 import java.io.File;
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.Scanner;
 
 public class App extends Application {
     public static void main(String[] args) {
 
-        Vertx vertx = Vertx.vertx();
-
-        HttpClientOptions options = new HttpClientOptions().
-                setProtocolVersion(HttpVersion.HTTP_2).
-                setSsl(true).
-                setUseAlpn(true).
-                setTrustAll(true);
-
-        HttpClient client = vertx.createHttpClient(options);
-
-        client.requestAbs(HttpMethod.GET, "http://localhost:8081/get_users",
-                result -> {
-                    System.out.println(result.statusCode());
-                    result.bodyHandler(body -> {
-                        System.out.println(body.toString());
-                    });
-                }).end();
         launch(args);
     }
     @Override
@@ -45,7 +18,7 @@ public class App extends Application {
         Parent root = FXMLLoader.load(new File("src/main/resources/sample.fxml").toURL());
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("Hello JavaFX");
+        stage.setTitle("Users List");
         stage.setWidth(250);
         stage.setHeight(200);
         stage.show();
--- JavaFX/src/main/java/eltex/tasks/User.java
+++ JavaFX/src/main/java/eltex/tasks/User.java
@@ -1,4 +1,18 @@
 package eltex.tasks;
 
+import lombok.*;
+
+@NoArgsConstructor
 public class User {
+
+    @Getter @Setter private Integer id;
+    @Getter @Setter private String fio;
+    @Getter @Setter private String phone;
+
+    User(Integer id, String fio, String phone) {
+        this.id = id;
+        this.fio = fio;
+        this.phone = phone;
+
+    }
 }
