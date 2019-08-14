abc2212f32d668b1fda0c1b4a2c5db5671b8c7d5
AlexeyQWE <mountain.skier@yadnex.ru>
2019-08-14 12:20:55 +0700

    Add sample.fxml

--- JavaFX/src/main/java/eltex/tasks/App.java
+++ JavaFX/src/main/java/eltex/tasks/App.java
@@ -1,25 +1,53 @@
 package eltex.tasks;
 
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpClient;
+import io.vertx.core.http.HttpClientOptions;
+import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.HttpVersion;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Group;
 import javafx.scene.text.Text;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.Scanner;
+
 public class App extends Application {
     public static void main(String[] args) {
+
+        Vertx vertx = Vertx.vertx();
+
+        HttpClientOptions options = new HttpClientOptions().
+                setProtocolVersion(HttpVersion.HTTP_2).
+                setSsl(true).
+                setUseAlpn(true).
+                setTrustAll(true);
+
+        HttpClient client = vertx.createHttpClient(options);
+
+        client.requestAbs(HttpMethod.GET, "http://localhost:8081/get_users",
+                result -> {
+                    System.out.println(result.statusCode());
+                    result.bodyHandler(body -> {
+                        System.out.println(body.toString());
+                    });
+                }).end();
         launch(args);
     }
     @Override
-    public void start(Stage stage) {
-// установка надписи
-        Text text = new Text("Hello from JavaFX!");
-        text.setLayoutY(80); text.setLayoutX(100);
-        Group group = new Group(text);
-        Scene scene = new Scene(group);
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(new File("src/main/resources/sample.fxml").toURL());
+        Scene scene = new Scene(root);
         stage.setScene(scene);
-        stage.setTitle("First Application");
-        stage.setWidth(300); stage.setHeight(250);
+        stage.setTitle("Hello JavaFX");
+        stage.setWidth(250);
+        stage.setHeight(200);
         stage.show();
     }
 }
